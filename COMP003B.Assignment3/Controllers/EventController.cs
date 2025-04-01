f9119114f67dad45c347de599990ae252fadb301
flores-97 <floresvictor15@gmail.com>
2025-04-01 11:39:15 -0700

    correct url show when links are clicked. Succes page has issues

--- COMP003B.Assignment3/Controllers/EventController.cs
+++ COMP003B.Assignment3/Controllers/EventController.cs
@@ -8,13 +8,15 @@ namespace COMP003B.Assignment3.Controllers
     {
         //Get: Register/EventCode
         [HttpGet("/event/register/spring-2025")]
+        [HttpGet("/event/register/fall-2025")]
+        [HttpGet("/Event/Register")]
         public IActionResult Register()
         {
             return View();
         }
 
 
-        [HttpPost("event/register")]
+        [HttpPost("Event/Register")]
         public IActionResult Register([FromForm] Register register)
         {
             if (!ModelState.IsValid)
@@ -24,7 +26,7 @@ namespace COMP003B.Assignment3.Controllers
             return RedirectToAction("Success", register);
         }
 
-        [Route("event/success")]
+        [Route("Event/Success")]
         public IActionResult Success(Register register)
         {
             return View(register);
